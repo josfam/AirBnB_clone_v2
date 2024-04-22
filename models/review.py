257b5420cb2e17dab0bbf79f61c61b388f06f4ff
josfam <josephamukun@tutanota.com>
2024-04-22 12:12:31 +0300

    Fix: Make `place_id` and `user_id` non - nullable

--- models/review.py
+++ models/review.py
@@ -11,6 +11,6 @@ class Review(BaseModel, Base):
 
     __tablename__ = 'reviews'
 
-    place_id = Column(String(60), ForeignKey('places.id'), nullable=True)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=True)
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     text = Column(String(1024), nullable=False)
