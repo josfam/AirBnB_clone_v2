d578e982b71156ef271688f0306b87c621a9d3e9
josfam <jamukun@proton.me>
2024-05-13 09:28:40 +0300

    Use pathlib for paths

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,13 +4,21 @@
 of the AirBnB Clone repo, using the function do_pack.
 """
 
+from pathlib import Path
 from fabric.api import local
 from datetime import datetime
 
 
 def do_pack():
     """Generates a .tgz archive from the contents of the web_static folder"""
-    local('mkdir -p versions')
+    versions_dir = Path('versions/')
+    if not Path.exists(versions_dir):
+        Path.mkdir(versions_dir)
     # year,month,day,hour,minute,second
     time = datetime.now().strftime('%y%m%d%I%M%S')
-    local(f'tar -cvf versions/web_static_{time}.tgz web_static')
+    tar_path = Path(versions_dir, f'web_static_{time}.tgz')
+    local(f'tar -cvf {tar_path} web_static')
+
+    if Path.exists(tar_path):
+        return(tar_path)
+    return None
