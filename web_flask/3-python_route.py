ae0bbfb35981b330581b72fa368f950bff66a5b7
josfam <jamukun@proton.me>
2024-05-18 16:34:10 +0000

    Implement `3. Python is cool!`

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+"""A Flask web application for the airbnb clone application"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    """The default home route when visiting the site"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """For the /hbnb route on the site"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """Display C, and whatever text was specified"""
+    return 'C {}'.format(text.replace("_", " "))
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text='is cool'):
+    """Display Python, and whatever text was specified"""
+    return 'Python {}'.format(text.replace("_", " "))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
